eb7d6162447a12e33172c03def74555a2fc09175
Aaron Lee <aaronlee232@gmail.com>
2023-06-11 23:15:27 -0500

    removed default hello endpoint and added documentation to custom example endpoint

--- client/pages/api/exampleEndpoint.ts
+++ client/pages/api/exampleEndpoint.ts
@@ -9,24 +9,36 @@ import { HttpStatusCode } from 'axios'
 import { NextApiRequest, NextApiResponse } from 'next'
 import { ApiError } from 'next/dist/server/api-utils'
 
+/**
+ *
+ * Endpoint used to test new middleware and central error handler (exception filter).
+ *
+ * Use this endpoint as a reference for creating new endpoints using middleware and exception filter
+ *
+ */
 const handler = async (
   req: NextApiRequest,
   res: NextApiResponse<ResponseData>
 ) => {
-  const dostuff = async () => {
-    throw new ApiError(HttpStatusCode.Forbidden, 'Error Handler Works!')
+  const handlerMainFunction = async () => {
+    // Uncomment this to test central error handling
+    // throw new ApiError(HttpStatusCode.Forbidden, 'Error Handler Works!')
+
+    // Used to test if connection to mongoDB is valid
     const result = await User.find()
 
     res.status(HttpStatusCode.Accepted).send({ ok: true, msg: result })
   }
 
+  // Loads specified middleware with handlerMainFunction. Will run in order specified.
   const middlewareLoadedHandler = withMiddleware(
     withMethodsGuard(['GET']),
     withRequestBodyGuard(),
     withMongoDBConnection(),
-    dostuff
+    handlerMainFunction
   )
 
+  // withExcpetionFilter wraps around the middleware-loaded handler to catch and handle any thrown errors in a centralized location
   return withExceptionFilter(req, res)(middlewareLoadedHandler)
 }
 
