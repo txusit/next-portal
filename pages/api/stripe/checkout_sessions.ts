765852222146247523787865046f051694af3e65
Aaron Lee <aaronlee232@gmail.com>
2023-07-27 00:09:15 -0500

    fixed webhook

--- pages/api/stripe/checkout_sessions.ts
+++ pages/api/stripe/checkout_sessions.ts
@@ -1,3 +1,4 @@
+import { supabase } from '@/lib/helpers/supabase'
 import withExceptionFilter from '@/lib/middleware/with-exception-filter'
 import withMethodsGuard from '@/lib/middleware/with-methods-guard'
 import withMiddleware from '@/lib/middleware/with-middleware'
@@ -6,6 +7,7 @@ import { ResponseData } from '@/types'
 import { CheckoutSessionSchema } from '@/types/endpoint-request-schemas'
 import { HttpStatusCode } from 'axios'
 import { NextApiRequest, NextApiResponse } from 'next'
+import { ApiError } from 'next/dist/server/api-utils'
 
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
 
@@ -16,7 +18,13 @@ const handler = async (
   const checkoutSession = async () => {
     const parsedBody = CheckoutSessionSchema.parse(req.body)
     const { selectedPriceId, email } = parsedBody
-    console.log('selectedPriceId:', selectedPriceId)
+
+    if (await isMembershipAlreadyPurchased(email, selectedPriceId)) {
+      throw new ApiError(
+        HttpStatusCode.Conflict,
+        'Unable to purchase because current membership covers the selected membership'
+      )
+    }
 
     // Create Checkout Sessions from body params.
     const session = await stripe.checkout.sessions.create({
@@ -44,6 +52,45 @@ const handler = async (
 
   // withExcpetionFilter wraps around the middleware-loaded handler to catch and handle any thrown errors in a centralized location
   return withExceptionFilter(req, res)(middlewareLoadedHandler)
+}
+
+const isMembershipAlreadyPurchased = async (
+  email: string,
+  selectedPriceId: string
+) => {
+  // Retrieve member id
+  const { data: member, error: fetchMemberError } = await supabase
+    .from('member')
+    .select('id')
+    .eq('email', email)
+    .single()
+  if (fetchMemberError) throw fetchMemberError
+
+  // Retrieve selected membership type
+  const { data: selectedMembership, error: fetchMembershipError } =
+    await supabase
+      .from('membership')
+      .select('type')
+      .eq('price_id', selectedPriceId)
+      .single()
+  if (fetchMembershipError) throw fetchMembershipError
+
+  // Retrieve past payment record types
+  const { data: paymentRecords, error: fetchPaymentRecordsError } =
+    await supabase
+      .from('payment_record')
+      .select('type')
+      .eq('member_id', member.id)
+  if (fetchPaymentRecordsError) throw fetchPaymentRecordsError
+
+  const MembershipTypesPaidFor = paymentRecords.map(
+    (membership) => membership.type
+  )
+
+  return (
+    MembershipTypesPaidFor.includes('full') ||
+    MembershipTypesPaidFor.includes(selectedMembership.type)
+  )
 }
 
 export default handler
