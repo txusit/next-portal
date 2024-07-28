4c49271b84c6d14bab6f9369ce4847d3f8ab2d18
Aaron Lee <aaronlee232@gmail.com>
2024-07-27 23:51:28 -0500

    Fixed Lint and sessionStorage Issues

--- pages/settings/layout.tsx
+++ pages/settings/layout.tsx
@@ -3,8 +3,8 @@ import { SidebarNav } from '@/components/common/settings/sidebar-nav'
 import { Button } from '@/components/ui/button'
 import { ChevronLeft } from 'lucide-react'
 import { useRouter } from 'next/router'
-import React from 'react'
-import { toast } from '@/components/ui/use-toast'
+import React, { useEffect, useState } from 'react'
+import { defaultUrl } from '@/config/nav'
 
 const sidebarNavItems = [
   {
@@ -31,14 +31,17 @@ interface SettingsLayoutProps {
 
 export default function SettingsLayout({ children }: SettingsLayoutProps) {
   const router = useRouter()
-  const lastNonSettingsPage = sessionStorage.getItem('lastNonSettingsPage')
+  const [lastNonSettingsPage, setLastNonSettingsPage] =
+    useState<string>(defaultUrl)
+
+  useEffect(() => {
+    setLastNonSettingsPage(
+      sessionStorage.getItem('lastNonSettingsPage') || defaultUrl
+    )
+  }, [])
 
   const handleBack = () => {
-    if (lastNonSettingsPage) {
-      router.replace(lastNonSettingsPage)
-    } else {
-      router.replace('/dashboard')
-    }
+    router.replace(lastNonSettingsPage)
   }
 
   return (
